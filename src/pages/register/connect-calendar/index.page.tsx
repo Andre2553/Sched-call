ebb019d483e119409c2b32ccf841bce260e53502
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-12-31 08:10:56 +1100

    Auth error handler

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,12 +1,22 @@
 import { Button, Heading, Text } from "@ignite-ui/react";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 // import { useRouter } from "next/router";
-import { ArrowRight } from "phosphor-react";
+import { ArrowRight, Check } from "phosphor-react";
 // import { api } from "../api/axios";
 import { Container, Header } from "../styles";
-import { ConnectBox, ConnectItem } from "./styles";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
 
 export default function Register() {
+  const session = useSession();
+  const router = useRouter();
+
+  const hasAuthError = !!router.query.error;
+  const idSignedIn = session.status === "authenticated";
+
+  async function handleConnectCalendar() {
+    await signIn("google");
+  }
   return (
     <Container>
       <Header>
@@ -16,11 +26,28 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" onClick={() => signIn("google")}>
-            Connect <ArrowRight />
-          </Button>
+          {idSignedIn ? (
+            <Button disabled>
+              Connected <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              onClick={() => handleConnectCalendar}
+              disabled={idSignedIn}
+            >
+              Connect
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
-        <Button type="submit">
+        {hasAuthError && (
+          <AuthError size="sm">
+            Failed to connect with your calendar. Please verify if you enabled
+            all permissions required.
+          </AuthError>
+        )}
+        <Button type="submit" disabled={!idSignedIn}>
           Continue <ArrowRight />
         </Button>
       </ConnectBox>
