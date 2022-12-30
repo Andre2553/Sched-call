7a9754ac8fe15625d6b67776b632b45b8e601199
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-12-30 13:05:13 +1100

    Form validation with Zod dev

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,15 +1,50 @@
-import { Button, TextInput } from "@ignite-ui/react";
+import { Button, Text, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
-import { Form } from "./styles";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormAnnotation } from "./styles";
+import { zodResolver } from "@hookform/resolvers/zod";
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .max(20, "Username must be at most 20 characters long")
+    .regex(/^([a-z\\-]+)$/i, "Only letters and dashes are allowed")
+    .transform((value) => value.toLowerCase()),
+});
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  });
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data);
+  }
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="schedcall.com/" placeholder="your-user" />
-      <Button size="sm" type="submit">
-        Claim Username
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="schedcall.com/"
+          placeholder="your-user"
+          {...register("username")}
+        />
+        <Button size="sm" type="submit">
+          Claim Username
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username ? errors.username.message : "Type a username"}
+        </Text>
+      </FormAnnotation>
+    </>
   );
 }
